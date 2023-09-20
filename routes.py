55efdb1978412d251c84b9ba18a79389f415e38c
mdakbari <manthanakbari999@gmail.com>
2023-09-20 21:58:20 +0530

    first commit

--- routes.py
+++ routes.py
@@ -65,55 +65,37 @@ def booking():
 
 
 #  Admin Route
-
-@main.route("/signup", methods=['GET','POST'])
-def signup():
-    if (request.method == 'POST'):
-        uname = request.form.get('uname')
-        email = request.form.get('email')
-        password = request.form.get('pass')
-        new_user = Signup(uname=uname,email=email)
-        new_user.set_password(password)
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template('login.html')
-    return render_template('register.html')
-
-
-
 @main.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         uname = request.form.get('uname')
         upassword = request.form.get('pass')
-        user = Signup.query.filter_by(uname=uname).first()
         
-        # if uname==params['admin_name'] and upassword==params['admin_password']:
-        #     session['user'] = uname
+        if uname==params['admin_name'] and upassword==params['admin_password']:
+            session['user'] = uname
+            return render_template('admin_layout.html', params=params)
 
-        if user and bcrypt.checkpw(upassword.encode('utf-8'), user.password.encode('utf-8')):
-            return render_template('admin_booking.html')
         return render_template("login.html")
     
     return render_template("login.html")
 
 @main.route("/admin")
 def admin():
-    return render_template("register.html")
+    return render_template("login.html")
 
 @main.route("/admin_booking")
 def admin_booking():
     if 'user' not in session:
         return redirect("login.html")
     book = Booking.query.all()
-    return render_template("admin_booking.html", book=book)
+    return render_template("admin_booking.html", book=book, params=params)
 
 @main.route("/admin_contact")
 def admin_contact():
     if 'user' not in session:
         return redirect("login.html")
     contact = Contact.query.all()
-    return render_template("admin_contact.html", contact=contact)
+    return render_template("admin_contact.html", contact=contact, params=params)
 
 @main.route("/admin_menu", methods=['GET','POST'])
 def admin_menu():
@@ -132,9 +114,53 @@ def admin_menu():
             image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         else:
             filename = None
+     
         new_food = Foods(name=name, price=price, category=category, description=description, image=filename)
         db.session.add(new_food)
         db.session.commit()
-        return render_template("admin_menu.html")
+
     food = Foods.query.all()
-    return render_template("admin_menu.html",  food=food)
+    return render_template("admin_menu.html",  food=food, params=params)
+
+
+@main.route("/edit_menu/<int:id>", methods=['GET', 'POST'])
+def edit_food(id):
+    if 'user' not in session:
+        return redirect("login.html")
+
+    edit_menu = Foods.query.get(id)
+
+    if request.method == 'POST':
+        edit_menu.name = request.form['name']
+        edit_menu.price = float(request.form['price'])
+        edit_menu.description = request.form['description']
+        edit_menu.category = request.form['category']
+
+        image_file = request.files['image']
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            edit_menu.image = filename
+
+        db.session.commit()
+
+        return redirect("/admin_menu")
+
+    return render_template("edit_menu.html", foods=edit_menu, params=params)
+
+
+@main.route("/delete_menu/<int:id>", methods=['GET', 'POST'])
+def delete_menu(id):
+    if 'user' not in session:
+        return redirect("login.html")
+    delete_menu = Foods.query.get(id)
+   
+    db.session.delete(delete_menu)
+    db.session.commit()
+
+    return redirect("/admin_menu")  
+
+@main.route("/logout")
+def logout():
+    session.pop('user') 
+    return redirect('login')  
